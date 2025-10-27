b4b315fca0d558f05f45eef361783da9cdad3d42
Alejandro Cerezo <alce65@hotmail.es>
2025-10-27 14:49:18 +0100

    Add getUsers con fetch y axios

---
+++ projects/demo0-TS/src/07-get-users.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export type User = {
+    id: string;
+    name: string;
+};
+
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+export const getUsers = async (): Promise<User[]> => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+export const getUserByAxios = async (): Promise<User[]> => {
+  const response = await axios.get<User[]>(url);
+  return response.data;
+}
